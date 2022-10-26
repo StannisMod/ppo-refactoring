8cf071465df028d57e791f4ab3580aebce89c81d
Quarter <stas.batalenkov@mail.ru>
2022-10-26 14:57:56 +0300

    Добавлен модифицирующий тест для драйвера БД

--- src/test/java/ru/akirakozov/sd/refactoring/DbDriverTest.java
+++ src/test/java/ru/akirakozov/sd/refactoring/DbDriverTest.java
@@ -7,6 +7,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DbDriverTest {
 
@@ -40,5 +43,26 @@ public class DbDriverTest {
         // test that GET receives three records
         assert counter[0] == 3;
         // and this is enough, because there is very unbelievable situation if data in records just change
+    }
+
+    @Test
+    public void testExecuteModify() {
+        DbDriver driver = new DbDriver("jdbc:sqlite:test.db");
+
+        driver.executeUpdate("INSERT INTO PRODUCT (name, price) VALUES ('monitor', 1100);");
+
+        final Set<String> expectedNames = Arrays.stream(new String[]{"apple", "coffee", "computer", "monitor"})
+                .collect(Collectors.toSet());
+        final Set<Integer> expectedPrices = Arrays.stream(new Integer[]{1, 10, 100, 1100})
+                .collect(Collectors.toSet());
+        driver.executeGet("SELECT * FROM PRODUCT", rs -> {
+            while (rs.next()) {
+                assert expectedNames.remove(rs.getString("name"));
+                assert expectedPrices.remove(rs.getInt("price"));
+            }
+        });
+
+        // test that GET receives four right records
+        assert expectedNames.size() == 0 && expectedPrices.size() == 0;
     }
 }
